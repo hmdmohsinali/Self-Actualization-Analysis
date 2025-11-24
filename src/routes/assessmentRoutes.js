b8ea14deec6d14b6455503328d17c9dd550942d5
BakarDevMaster <bakardevmaster@gmail.com>
2025-11-24 13:18:56 +0500

    download PDF endpoint

--- src/routes/assessmentRoutes.js
+++ src/routes/assessmentRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { submitAssessment, getLatestAssessment } from "../controllers/assessmentController.js";
+import { submitAssessment, getLatestAssessment, downloadAssessmentPDF } from "../controllers/assessmentController.js";
 import { authenticate } from "../middlewares/auth.js";
 
 const router = express.Router();
 
 router.post("/submit", authenticate, submitAssessment);
 router.get("/result", authenticate, getLatestAssessment);
-
+router.get("/download-pdf", authenticate, downloadAssessmentPDF);
 
 export default router;
 
